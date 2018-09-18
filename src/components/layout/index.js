ae408d5c1e0ebc54540a5e6d1936b54153531660
Oliver <oleksii@mobilebridge.com>
2018-09-18 10:38:29 +0200

    upgrade to gatsby 2.0

---
+++ src/components/layout/index.js
@@ -0,0 +1,58 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import { StaticQuery, graphql } from "gatsby"
+
+import Header from '../header/header';
+import Body from '../body/body';
+import Footer from '../footer/footer';
+import './index.css';
+import './grid.css';
+
+const Layout = ({ children, data }) => (
+  <StaticQuery
+    query={graphql`
+      query LayoutQuery {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Fragment>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Noto+Sans" rel="stylesheet"></link>
+        </Helmet>
+        <Header />
+        <Body>
+          {children}
+        </Body>
+        <Footer />
+      </Fragment>
+    )}
+  />
+)
+
+Layout.propTypes = {
+  children: PropTypes.element,
+}
+
+export default Layout
+
+export const query = graphql`
+  query SiteTitleQuery {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+  }
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Layout from '../components/layout';
 import styles from './styles/index.module.css';
 import { Parser } from 'htmlparser2';
-import Link from 'gatsby-link'
 
 function getExcerpt(html) {
   const excerptLength = 350;
@@ -18,31 +18,33 @@ function getExcerpt(html) {
 
 const IndexPage = ({ data }) => {
   return (
-  <div className="row">
-    <div className="col col-4-of-12">
-      <h1>About us</h1>
-      <p>There’re fresh vacancies every month.</p>
-      <p>Make sure you've read Writing a comprehensive résumé article.</p>
-      <p><a href="https://facebook.com/">Follow us on Facebook</a></p>
-      <h1>Fresh updates are here!</h1>
-      <p>We post the most recent vacancies in our Facebook group and in the Telegram channel.</p>
-      <p><a href="https://t.me/">Join Telegram channel</a></p>
-    </div>
-    <div className={`${styles.right} col col-8-of-12`}>
-      <h1>Vacancies {data.allMarkdownRemark.totalCount}</h1>
-      <div className="row row-nested">
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-            <div className="col col-4-of-12" key={node.id}>
-              <h3>{node.frontmatter.title}</h3>
-              <div>
-                {node.frontmatter.tags.split(", ").map(tag => <div key={tag}>{tag}</div>)}
-              </div>
-              <div>{getExcerpt(node.html)}</div>
-            </div>
-        ))}
+    <Layout>
+      <div className="row">
+        <div className="col col-4-of-12">
+          <h1>About us</h1>
+          <p>There’re fresh vacancies every month.</p>
+          <p>Make sure you've read Writing a comprehensive résumé article.</p>
+          <p><a href="https://facebook.com/">Follow us on Facebook</a></p>
+          <h1>Fresh updates are here!</h1>
+          <p>We post the most recent vacancies in our Facebook group and in the Telegram channel.</p>
+          <p><a href="https://t.me/">Join Telegram channel</a></p>
+        </div>
+        <div className={`${styles.right} col col-8-of-12`}>
+          <h1>Vacancies {data.allMarkdownRemark.totalCount}</h1>
+          <div className="row row-nested">
+            {data.allMarkdownRemark.edges.map(({ node }) => (
+                <div className="col col-4-of-12" key={node.id}>
+                  <h3>{node.frontmatter.title}</h3>
+                  <div>
+                    {node.frontmatter.tags.split(", ").map(tag => <div key={tag}>{tag}</div>)}
+                  </div>
+                  <div>{getExcerpt(node.html)}</div>
+                </div>
+            ))}
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
+    </Layout>
 )}
 
 export default IndexPage;
