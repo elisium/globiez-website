91afe9852c5dee1b67632e973c3f4b44595b1540
Oliver <oleksii@mobilebridge.com>
2018-09-18 14:16:46 +0200

    primal home page layout done

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -2,18 +2,22 @@ import React from 'react';
 import styles from './footer.module.css';
 
 const Footer = ({ children }) => (
-    <div className={styles.main}>
+    <footer className={styles.main}>
         <div className="container">
             <div className="row">
-                <div className="col col-4-of-12">
-                    <p>Social media: Facebook, LinkedIn</p>
+                <div className={`col col-4-of-12 ${styles.left}`}>
+                    <div><p>Social media:</p></div>
+                    <div>
+                        <p><a href="https://facebook.com/" target="_blank" rel="noopener noreferrer">Facebook</a></p>
+                        <p><a href="https://linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a></p>
+                    </div>
                 </div>
-                <div className="col col-8-of-12">
-                    <p>Email: <a href="mailto:hello@globiez.com">hello@globiez.com</a></p>
+                <div className={`col col-8-of-12 col-4-of-8 ${styles.right}`}>
+                    <p>Email: <a className={styles.linkMail} href="mailto:hello@globiez.com">hello@globiez.com</a></p>
                 </div>
             </div>
         </div>
-    </div>
+    </footer>
 )
 
 export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
+import TelegramButton from '../components/shared/telegramButton';
 import styles from './styles/index.module.css';
 import { Parser } from 'htmlparser2';
 
@@ -20,23 +21,23 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <div className="row">
-        <div className="col col-4-of-12">
+        <aside className="left-panel col col-4-of-12 col-4-of-8">
           <h1>About us</h1>
           <p>There’re fresh vacancies every month.</p>
           <p>Make sure you've read Writing a comprehensive résumé article.</p>
-          <p><a href="https://facebook.com/">Follow us on Facebook</a></p>
+          <p><a href="https://facebook.com/" target="_blank" rel="noopener noreferrer">Follow us on Facebook</a></p>
           <h1>Fresh updates are here!</h1>
           <p>We post the most recent vacancies in our Facebook group and in the Telegram channel.</p>
-          <p><a href="https://t.me/">Join Telegram channel</a></p>
-        </div>
-        <div className={`${styles.right} col col-8-of-12`}>
+          <p><TelegramButton /></p>
+        </aside>
+        <div className="right-panel col col-8-of-12 col-4-of-8">
           <h1>Vacancies {data.allMarkdownRemark.totalCount}</h1>
           <div className="row row-nested">
             {data.allMarkdownRemark.edges.map(({ node }) => (
-                <div className="col col-4-of-12" key={node.id}>
-                  <h3>{node.frontmatter.title}</h3>
-                  <div>
-                    {node.frontmatter.tags.split(", ").map(tag => <div key={tag}>{tag}</div>)}
+                <div className={`col col-4-of-12 col-4-of-8 ${styles.vacancie}`} key={node.id}>
+                  <h3 className={styles.vacancie_header}>{node.frontmatter.title}</h3>
+                  <div className={styles.tags}>
+                    {node.frontmatter.tags.split(", ").map(tag => <div className={styles.tag} key={tag}>{tag}</div>)}
                   </div>
                   <div>{getExcerpt(node.html)}</div>
                 </div>
